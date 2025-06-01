f3b608e36f04c7a3f8a9ddd4a726e28034554cb1
Pierre-Yves PAMART <pypamart@gmail.com>
2025-06-01 23:29:28 +0200

    📝 Update docstring for Container class and improve formatting of api_data_url

--- src/dpetools/config/container.py
+++ src/dpetools/config/container.py
@@ -1,9 +1,13 @@
-from typing import Callable
+"""
+Container class for configuration settings.
+"""
 
-from dpetools.api_client import DPEApiClient
 
 class Container:
     """
-    A class to represent a container for configuration settings.
+    A class to represent a container for configuration settings and dependencies injection.
     """
-    api_data_url: str = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe03existant/lines"
+
+    api_data_url: str = (
+        "https://data.ademe.fr/data-fair/api/v1/datasets/dpe03existant/lines"
+    )
