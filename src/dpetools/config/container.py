a6cd76fb9b2a9e50f71e8317e4652b5e1cacb34d
Pierre-Yves PAMART <pypamart@gmail.com>
2025-06-01 23:26:08 +0200

    🗑️ Remove unused dpe_client_factory method from Container class

--- src/dpetools/config/container.py
+++ src/dpetools/config/container.py
@@ -7,8 +7,3 @@ class Container:
     A class to represent a container for configuration settings.
     """
     api_data_url: str = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe03existant/lines"
-    
-    @staticmethod
-    def dpe_client_factory() -> DPEApiClient:
-        """Factory method to create a DPEAPIClient instance."""
-        return DPEApiClient(Container.api_data_url)
