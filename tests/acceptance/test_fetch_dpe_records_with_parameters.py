c547c2ab86cc999893b3960b02e9c287d1d7c590
Pierre-Yves PAMART <pypamart@gmail.com>
2025-07-14 22:38:12 +0200

    ✨ Implement rule 2 accpetances tests

--- tests/acceptance/test_fetch_dpe_records_with_parameters.py
+++ tests/acceptance/test_fetch_dpe_records_with_parameters.py
@@ -1,13 +1,22 @@
+from typing import Any
+
 import pandas as pd
 import pytest
 from pytest_bdd import given, parsers, scenarios, then, when
 
 from dpetools.api_client import DPEApiClient
 from dpetools.config.container import Container
-from dpetools.exceptions import InvalidDPERecordsLimitError
+from dpetools.exceptions import InvalidDPERecordsLimitError, NonExistingColumnError
 
 # Link the .feature file
 scenarios("../../docs/features/fetch_dpe_records_with_parameters.feature")
+
+# ███████╗██╗██╗  ██╗████████╗██╗   ██╗██████╗ ███████╗███████╗
+# ██╔════╝██║╚██╗██╔╝╚══██╔══╝██║   ██║██╔══██╗██╔════╝██╔════╝
+# █████╗  ██║ ╚███╔╝    ██║   ██║   ██║██████╔╝█████╗  ███████╗
+# ██╔══╝  ██║ ██╔██╗    ██║   ██║   ██║██╔══██╗██╔══╝  ╚════██║
+# ██║     ██║██╔╝ ██╗   ██║   ╚██████╔╝██║  ██║███████╗███████║
+# ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝
 
 
 @pytest.fixture(scope="session")
@@ -15,8 +24,9 @@ def dpe_api_client() -> DPEApiClient:
     """
     Fixture to create a DPEAPIClient instance for testing.
     """
-    api_data_url = Container.api_data_url
-    return DPEApiClient(api_data_url=api_data_url)
+    api_data_url = Container.API_DATA_URL
+    api_schema_url = Container.API_SCHEMA_URL
+    return DPEApiClient(api_data_url=api_data_url, api_schema_url=api_schema_url, timeout=Container.REQUESTS_TIMEOUT)
 
 
 @pytest.fixture
@@ -25,6 +35,14 @@ def context() -> dict[str, str]:
     Fixture to provide a context dictionary for storing shared data between steps.
     """
     return {}
+
+
+#  ██████╗ ██╗██╗   ██╗███████╗███╗   ██╗    ███████╗████████╗███████╗██████╗ ███████╗
+# ██╔════╝ ██║██║   ██║██╔════╝████╗  ██║    ██╔════╝╚══██╔══╝██╔════╝██╔══██╗██╔════╝
+# ██║  ███╗██║██║   ██║█████╗  ██╔██╗ ██║    ███████╗   ██║   █████╗  ██████╔╝███████╗
+# ██║   ██║██║╚██╗ ██╔╝██╔══╝  ██║╚██╗██║    ╚════██║   ██║   ██╔══╝  ██╔═══╝ ╚════██║
+# ╚██████╔╝██║ ╚████╔╝ ███████╗██║ ╚████║    ███████║   ██║   ███████╗██║     ███████║
+#  ╚═════╝ ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═══╝    ╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚══════╝
 
 
 @given("the ADEME DPE API is reachable")
@@ -36,7 +54,15 @@ def given_api_is_reachable(dpe_api_client: DPEApiClient) -> None:
     assert dpe_api_client.is_api_reachable()
 
 
-@when(parsers.parse("I fetch DPE records with limit {nbrecords:d}"), target_fixture="dpe_records_dataframe")
+# ██╗    ██╗██╗  ██╗███████╗███╗   ██╗    ███████╗████████╗███████╗██████╗ ███████╗
+# ██║    ██║██║  ██║██╔════╝████╗  ██║    ██╔════╝╚══██╔══╝██╔════╝██╔══██╗██╔════╝
+# ██║ █╗ ██║███████║█████╗  ██╔██╗ ██║    ███████╗   ██║   █████╗  ██████╔╝███████╗
+# ██║███╗██║██╔══██║██╔══╝  ██║╚██╗██║    ╚════██║   ██║   ██╔══╝  ██╔═══╝ ╚════██║
+# ╚███╔███╔╝██║  ██║███████╗██║ ╚████║    ███████║   ██║   ███████╗██║     ███████║
+#  ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝    ╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚══════╝
+
+
+@when(parsers.parse("I fetch DPE records with a limit of {nbrecords:d}"), target_fixture="dpe_records_dataframe")
 @when("I fetch DPE records without specifying a limit", target_fixture="dpe_records_dataframe")
 def when_fetch_with_limit(dpe_api_client: DPEApiClient, nbrecords: int | None = None) -> pd.DataFrame:
     """
@@ -47,18 +73,8 @@ def when_fetch_with_limit(dpe_api_client: DPEApiClient, nbrecords: int | None = 
         dpe_records_dataframe = dpe_api_client.fetch_dpe_records()
     else:
         dpe_records_dataframe = dpe_api_client.fetch_dpe_records(nbrecords=nbrecords)
-    
+
     return dpe_records_dataframe
-
-
-@then(parsers.parse("I should receive exactly {nbrecords:d} records"))
-def then_receive_exactly_n_records(nbrecords: int, dpe_records_dataframe: pd.DataFrame) -> None:
-    """
-    Assert that the number of records received matches the expected count.
-    """
-    assert len(dpe_records_dataframe) == nbrecords, (
-        f"Expected {nbrecords} records, but received {len(dpe_records_dataframe)}"
-    )
 
 
 @when(
@@ -72,18 +88,6 @@ def when_fetch_with_negative_limit(dpe_api_client: DPEApiClient, nbrecords: int)
     with pytest.raises(InvalidDPERecordsLimitError) as exc_info:
         dpe_api_client.fetch_dpe_records(nbrecords=nbrecords)
     return exc_info
-
-
-@then("I should receive an error indicating the limit must be a positive integer")
-def then_error_limit_positive_integer(fetch_error):
-    """
-    Assert that the error message indicates the limit must be a positive integer.
-
-    Args:
-        fetch_error (pytest.ExceptionInfo): The exception information captured during the fetch attempt.
-
-    """
-    assert "positive integer" in str(fetch_error.value)
 
 
 @when(parsers.parse('I fetch DPE records sorted by "{field}" in {order} order'), target_fixture="dpe_records_dataframe")
@@ -105,7 +109,74 @@ def when_fetch_sorted_element(dpe_api_client: DPEApiClient, field: str, order: s
     return dpe_records_dataframe
 
 
-@then(parsers.parse('the records should be ordered from oldest to newest by "{field}"'))
+@when(parsers.parse('I fetch DPE records sorted by "{field}"'), target_fixture="fetch_error")
+def when_fetch_sorted_by_invalid_field(dpe_api_client: DPEApiClient, field: str) -> pytest.ExceptionInfo:
+    """
+    Attempt to fetch DPE records sorted by a non-existent field and capture the exception.
+
+    Args:
+        dpe_api_client (DPEApiClient): The API client to fetch records.
+        field (str): The field by which to sort the records, which does not exist.
+
+    Returns:
+        pytest.ExceptionInfo: The exception information captured during the fetch attempt.
+    """
+    with pytest.raises(NonExistingColumnError) as exc_info:
+        dpe_api_client.fetch_dpe_records(sort_by=field, nbrecords=10)
+    return {"exc_info": exc_info, "field": field}
+
+
+@when(
+    parsers.parse('I fetch DPE records selecting columns "{col_name_1}" and "{col_name_2}"'),
+    target_fixture="dpe_records_dataframe",
+)
+def when_fetch_selecting_columns(dpe_api_client: DPEApiClient, col_name_1: str, col_name_2: str) -> pd.DataFrame:
+    """
+    Fetch DPE records selecting specific columns.
+
+    Args:
+        dpe_api_client (DPEApiClient): The API client to fetch records.
+        col_name_1 (str): The first column to select.
+        col_name_2 (str): The second column to select.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the selected columns from the DPE records.
+    """
+    return dpe_api_client.fetch_dpe_records(select_columns=[col_name_1, col_name_2])
+
+
+# ████████╗██╗  ██╗███████╗███╗   ██╗    ███████╗████████╗███████╗██████╗ ███████╗
+# ╚══██╔══╝██║  ██║██╔════╝████╗  ██║    ██╔════╝╚══██╔══╝██╔════╝██╔══██╗██╔════╝
+#    ██║   ███████║█████╗  ██╔██╗ ██║    ███████╗   ██║   █████╗  ██████╔╝███████╗
+#    ██║   ██╔══██║██╔══╝  ██║╚██╗██║    ╚════██║   ██║   ██╔══╝  ██╔═══╝ ╚════██║
+#    ██║   ██║  ██║███████╗██║ ╚████║    ███████║   ██║   ███████╗██║     ███████║
+#    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝    ╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚══════╝
+
+
+@then(parsers.parse("exactly {nbrecords:d} records are returned"))
+@then(parsers.parse("exactly {nbrecords:d} records are returned by default"))
+def then_receive_exactly_n_records(nbrecords: int, dpe_records_dataframe: pd.DataFrame) -> None:
+    """
+    Assert that the number of records received matches the expected count.
+    """
+    assert len(dpe_records_dataframe) == nbrecords, (
+        f"Expected {nbrecords} records, but received {len(dpe_records_dataframe)}"
+    )
+
+
+@then("I should receive an error indicating the limit must be a positive integer")
+def then_error_limit_positive_integer(fetch_error):
+    """
+    Assert that the error message indicates the limit must be a positive integer.
+
+    Args:
+        fetch_error (pytest.ExceptionInfo): The exception information captured during the fetch attempt.
+
+    """
+    assert "positive integer" in str(fetch_error.value)
+
+
+@then(parsers.parse('the records are ordered from oldest to newest by "{field}"'))
 def then_records_ordered_oldest_to_newest(dpe_records_dataframe: pd.DataFrame, field: str) -> None:
     """
     Assert that the records are ordered from oldest to newest by the specified field.
@@ -119,7 +190,7 @@ def then_records_ordered_oldest_to_newest(dpe_records_dataframe: pd.DataFrame, f
     )
 
 
-@then(parsers.parse('the records should be ordered from newest to oldest by "{field}"'))
+@then(parsers.parse('the records are ordered from newest to oldest by "{field}"'))
 def then_records_ordered_newest_to_oldest(dpe_records_dataframe: pd.DataFrame, field: str) -> None:
     """
     Assert that the records are ordered from newest to oldest by the specified field.
@@ -131,36 +202,43 @@ def then_records_ordered_newest_to_oldest(dpe_records_dataframe: pd.DataFrame, f
     assert dpe_records_dataframe[field].is_monotonic_decreasing, (
         f"The records are not ordered by '{field}' in descending order."
     )
-    
-@when(parsers.parse('I fetch DPE records sorted by "{field}" that does not exists'), target_fixture="fetch_error")
-def when_fetch_sorted_by_invalid_field(dpe_api_client: DPEApiClient, field: str) -> pytest.ExceptionInfo:
-    """
-    Attempt to fetch DPE records sorted by a non-existent field and capture the exception.
 
-    Args:
-        dpe_api_client (DPEApiClient): The API client to fetch records.
-        field (str): The field by which to sort the records, which does not exist.
 
-    Returns:
-        pytest.ExceptionInfo: The exception information captured during the fetch attempt.
-    """
-    with pytest.raises(InvalidDPERecordsLimitError) as exc_info:
-        dpe_api_client.fetch_dpe_records(sort_by=field, nbrecords=10)
-    return exc_info
-
-@then("I should receive an error indicating the sort field is invalid")
-def then_error_sort_field_invalid(fetch_error: pytest.ExceptionInfo) -> None:
+@then("an error is returned indicating the sort field is invalid")
+def then_error_sort_field_invalid(fetch_error: dict[str, Any]) -> None:
     """
     Assert that the error message indicates the sort field is invalid.
 
     Args:
         fetch_error (pytest.ExceptionInfo): The exception information captured during the fetch attempt.
     """
-    assert "Bad request" in str(fetch_error.value), (
-        f"Expected error message to contain 'sort field is invalid', but got: {fetch_error.value}"
+    # assert "Bad request" in str(fetch_error.value), (
+    #     f"Expected error message to contain 'sort field is invalid', but got: {fetch_error.value}"
+    # )
+    error: pytest.ExceptionInfo = fetch_error["exc_info"]
+    field_name = fetch_error["field"]
+
+    assert str(error.value).startswith(
+        f"The requested column(s) ['{field_name}'] do not exist in the available columns:"
+    ), f"Expected error message to indicate non-existing columns, but got: {error.value}"
+
+
+@then(parsers.parse('each returned record contains only the fields "{col_name_1}" and "{col_name_2}"'))
+def then_only_selected_fields(dpe_records_dataframe: pd.DataFrame, col_name_1: str, col_name_2: str) -> None:
+    """
+    Assert that each record in the DataFrame contains only the specified fields.
+
+    Args:
+        dpe_records_dataframe (pd.DataFrame): The DataFrame containing the fetched DPE records.
+        col_name_1 (str): The first column to check.
+        col_name_2 (str): The second column to check.
+    """
+    expected_columns = [col_name_1, col_name_2]
+    actual_columns = list(dpe_records_dataframe.columns)
+
+    assert set(expected_columns) - set(actual_columns) == set(), (
+        f"Expected columns {expected_columns} not found in the DataFrame. Actual columns: {actual_columns}."
     )
-# @when(parsers.parse('I fetch DPE records selecting columns "{col1}" and "{col2}"'))
-# def when_fetch_selecting_columns(_=None): ...
 
 
 # @when("I fetch DPE records without specifying columns to select")
